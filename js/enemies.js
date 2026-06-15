11b220d57ac904471a70cd2337a5dc1b66d9a103
ceddexheg <chegeduss188@gmail.com>
2026-06-15 11:57:47 +0200

    Refactor enemy types and add damage and sprite properties

--- js/enemies.js
+++ js/enemies.js
@@ -3,11 +3,12 @@
 // ===============================
 
 const ENEMY_TYPES = {
-    soldier: { hp: 100, speed: 2, money: 1, stealth : false , armored : false },
-    armored: { hp: 250, speed: 1.5, money: 5, stealth : false , armored : true},
-    stealth: { hp: 50, speed: 4, money: 10, stealth : true , armored : false},
-    cyborg: { hp: 300, speed: 2.5, money: 100, stealth : true , armored : true},
+    soldier: { hp: 100, speed: 4, money: 1, damage: 1, stealth: false, armored: false, sprite: merzwegImg },
+    armored: { hp: 250, speed: 3, money: 5, damage: 2, stealth: false, armored: true, sprite: juggernautImg },
+    stealth: { hp: 50, speed: 7, money: 10, damage: 5, stealth: true, armored: false, sprite: ninjaImg },
+    cyborg: { hp: 300, speed: 3, money: 100, damage: 10, stealth: true, armored: true, sprite: cyborgImg },
 };
+
 
 // ===============================
 // 👾 FACTORY
@@ -20,10 +21,19 @@ function createEnemy(type, path) {
         x: path[0].x,
         y: path[0].y,
         targetIndex: 1,
+
         hp: e.hp,
         speed: e.speed,
+
         stealth: e.stealth,
         armored: e.armored,
+
+        sprite: e.sprite,
+        type: type,
+
+        reward: e.money,
+        damage: e.damage,
+
         dead: false,
         angle: 0
     };
