e759d8285a99f1e6165fbf6cc07a5b10d9c0b5e7
ceddexheg <chegeduss188@gmail.com>
2026-06-27 14:04:22 +0200

    Update assets.js

--- js/assets.js
+++ js/assets.js
@@ -14,28 +14,27 @@ const dirtImg            = loadImage("assets/Pfad.png");
 const dirtcornerImg      = loadImage("assets/Pfad_Ecke.png");
 
 // Enemies
-const soldierImg         = loadImage("assets/towerDefense_tile245.png");
-const juggernautImg      = loadImage("assets/towerDefense_tile246.png");
-const ninjaImg           = loadImage("assets/towerDefense_tile247.png");
-const cyborgImg          = loadImage("assets/towerDefense_tile248.png");
+const soldierImg         = loadImage("assets/merz.png");
+const juggernautImg      = loadImage("assets/putin.png");
+const ninjaImg           = loadImage("assets/schroeder.png");
+const cyborgImg          = loadImage("assets/trump.png");
 
 // Towers
 const cannonImg          = loadImage("assets/MG.png");
 const doublecannonImg    = loadImage("assets/Doppelmg.png");
-const rocketImg          = loadImage("assets/Raketenturm.png");
-const bigrocketImg       = loadImage("assets/Großerraketenturm.png");
-const planeImg           = loadImage("assets/towerDefense_tile270.png");
-const heavyplaneImg      = loadImage("assets/towerDefense_tile271.png");
+const rocketImg          = loadImage("assets/Raketentuk.png");
+const bigrocketImg       = loadImage("assets/Raketentruck.png");
+const planeImg           = loadImage("assets/Flugzeug.png");
+const heavyplaneImg      = loadImage("assets/Jet.png");
 const MBTtopImg          = loadImage("assets/Kanone.png");
 const MBTbottomImg       = loadImage("assets/towerDefense_tile268.png");
-const APCtopImg          = loadImage("assets/towerDefense_tile292.png");
+const APCtopImg          = loadImage("assets/APC.png");
 const APCbottomImg       = loadImage("assets/towerDefense_tile269.png");
-const CWISImg            = loadImage("assets/towerDefense_tile204.png");
+const CWISImg            = loadImage("assets/Flak.png");
 
 // Projectiles
 const cannonProjectileImg        = loadImage("assets/Bullet.png");
 const doublecannonProjectileImg  = loadImage("assets/Granate.png");
-const rocketProjectileImg        = loadImage("assets/Rakete_an1.png");
-const rocketProjectileImgalt     = loadImage("assets/Rakete_an2.png");
-const bigrocketProjectileImg     = loadImage("assets/Großerakete_an1.png");
-const bigrocketProjectilealtImg  = loadImage("assets/Großerakete_an2.png");
+const rocketProjectileImg        = loadImage("assets/Raketeklein.png");
+const bigrocketProjectileImg     = loadImage("assets/Großerakete.png");
+const explosionImg               = loadImage("assets/Explosion.png");
